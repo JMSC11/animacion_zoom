af9446215d69f16367a3ebc49d3400ba93a777d8
MisaelSc <misaelsc.ms@gmail.com>
2023-12-06 14:15:14 -0600

    agregando touchmove y observer

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ var minScale = 1; // Mínimo zoom permitido
 var lastTouchY = 0;
 
 function updateScale(delta) {
+    // Ajusta el zoom basado en la dirección del scroll o el desplazamiento táctil
     if (delta < 0) {
         scale *= 0.98; // Disminuir el zoom
     } else {
@@ -22,19 +23,42 @@ function updateScale(delta) {
     contenido.style.opacity = scale === maxScale ? '1' : '0';
 }
 
+// Evento de scroll para computadoras
 window.addEventListener('wheel', function(e) {
     updateScale(e.deltaY);
 });
 
+// Eventos táctiles para dispositivos móviles
 window.addEventListener('touchstart', function(e) {
     lastTouchY = e.touches[0].clientY;
 });
 
 window.addEventListener('touchmove', function(e) {
-    e.preventDefault(); // Prevenir el scroll predeterminado del navegador
     var touchY = e.touches[0].clientY;
     var delta = lastTouchY - touchY;
     lastTouchY = touchY;
 
     updateScale(delta);
 }, { passive: false });
+
+// IntersectionObserver para un efecto adicional o de respaldo
+var observerOptions = {
+  root: null,
+  threshold: 0.5
+};
+
+var observer = new IntersectionObserver(function(entries) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Aplicar el zoom máximo cuando el elemento es visible
+      imagenPantalla.style.transform = 'scale(' + maxScale + ')';
+      contenido.style.opacity = 1;
+    } else {
+      // Restablecer cuando no es visible
+      imagenPantalla.style.transform = 'scale(1)';
+      contenido.style.opacity = 0;
+    }
+  });
+}, observerOptions);
+
+observer.observe(document.querySelector('.contenedor-principal'));
